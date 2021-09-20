840914501945c155721ef1e82d534b2ced7f02d9
dulovici <igorx24@gmail.com>
2021-09-20 15:44:02 +0200

    v1.0

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 
 
-console.log('dev')
+(function dropingCards(){
+'use strict'
+    
+
+const articles = [...document.querySelectorAll('li')];
+const questions = [...document.querySelectorAll('.question')];
+const icons = [...document.querySelectorAll('.icon')];
+const answers = [...document.querySelectorAll('.answer')];
+
+function openQuestion(e) {
+    if(this.firstElementChild.className === 'question selected') {
+    this.firstElementChild.classList.toggle("selected");
+    this.firstElementChild.lastElementChild.classList.toggle("flip");
+    this.lastElementChild.classList.toggle('show');
+    }
+    else {
+    questions.forEach(el => el.classList.remove('selected'));
+    icons.forEach(el => el.classList.remove('flip'));
+    answers.forEach(el => el.classList.remove('show'));
+
+    this.firstElementChild.classList.toggle("selected");
+    this.firstElementChild.lastElementChild.classList.toggle("flip");
+    this.lastElementChild.classList.toggle('show');
+    }
+
+}
+
+articles.forEach(question => {
+    question.addEventListener('click', openQuestion)
+})
+
+    
+})()
+
+
+
