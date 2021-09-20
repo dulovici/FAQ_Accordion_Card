99644f177d5508b8c66e14df4fcb1f11123d8127
dulovici <igorx24@gmail.com>
2021-09-20 18:51:21 +0200

    clean-up

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
 
-
 (function dropingCards(){
 'use strict'
     
+    const articles = [...document.querySelectorAll('li')];
+    const questions = [...document.querySelectorAll('.question')];
+    const icons = [...document.querySelectorAll('.icon')];
+    const answers = [...document.querySelectorAll('.answer')];
 
-const articles = [...document.querySelectorAll('li')];
-const questions = [...document.querySelectorAll('.question')];
-const icons = [...document.querySelectorAll('.icon')];
-const answers = [...document.querySelectorAll('.answer')];
 
-
-function toggleClasses(element) {
-    element.firstElementChild.classList.toggle("selected");
-    element.firstElementChild.lastElementChild.classList.toggle("flip");
-    element.lastElementChild.classList.toggle('show');
-}
-
-function openQuestion() {
-    if(this.firstElementChild.className === 'question selected') {
-    toggleClasses(this)
+    function toggleClasses(element) {
+        element.firstElementChild.classList.toggle("selected");
+        element.firstElementChild.lastElementChild.classList.toggle("flip");
+        element.lastElementChild.classList.toggle('show');
     }
 
-    else {
-    questions.forEach(el => el.classList.remove('selected'));
-    icons.forEach(el => el.classList.remove('flip'));
-    answers.forEach(el => el.classList.remove('show'));
-    toggleClasses(this)
+    function openQuestion() {
+        if(this.firstElementChild.className === 'question selected') {
+        toggleClasses(this)
+        }
+
+        else {
+        questions.forEach(el => el.classList.remove('selected'));
+        icons.forEach(el => el.classList.remove('flip'));
+        answers.forEach(el => el.classList.remove('show'));
+        toggleClasses(this)
+        }
     }
-}
 
-articles.forEach(question => {
-    question.addEventListener('click', openQuestion)
-})
+    articles.forEach(question => {
+        question.addEventListener('click', openQuestion)
+    })
 
-    
 })()
 
 
