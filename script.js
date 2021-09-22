580df7ccb5293fd715e777e19868d93b6f8bb46c
dulovici <igorx24@gmail.com>
2021-09-22 16:56:00 +0200

    js trim

--- script.js
+++ script.js
@@ -3,28 +3,16 @@
 'use strict'
     
     const articles = [...document.querySelectorAll('li')];
-    const questions = [...document.querySelectorAll('.question')];
-    const icons = [...document.querySelectorAll('.icon')];
-    const answers = [...document.querySelectorAll('.answer')];
-
-
-    function toggleClasses(element) {
-        element.firstElementChild.classList.toggle("selected");
-        element.firstElementChild.lastElementChild.classList.toggle("flip");
-        element.lastElementChild.classList.toggle('show');
-    }
 
     function openQuestion() {
-        if(this.firstElementChild.className === 'question selected') {
-        toggleClasses(this)
+        if(this.className === 'active') {
+            this.classList.toggle('active')
+        }
+        else {
+            articles.forEach(el => el.classList.remove('active'))
+            this.classList.add("active")
         }
 
-        else {
-        questions.forEach(el => el.classList.remove('selected'));
-        icons.forEach(el => el.classList.remove('flip'));
-        answers.forEach(el => el.classList.remove('show'));
-        toggleClasses(this)
-        }
     }
 
     articles.forEach(question => {
