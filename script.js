f21059ca9dcc32c41baf6395d852fa434014708f
dulovici <81692156+dulovici@users.noreply.github.com>
2021-09-28 12:03:59 +0200

    Dev (#2)
    * dev init
    
    * seting up dev
    
    * v1.0
    
    * v1.1
    
    * clean-up
    
    * open/close transition added
    
    * cleanup
    
    * trim
    
    * js trim

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 
+(function dropingCards(){
+'use strict'
+    
+    const articles = [...document.querySelectorAll('li')];
 
-console.log('main')
+    function openQuestion() {
+        if(this.className === 'active') {
+            this.classList.toggle('active')
+        }
+        else {
+            articles.forEach(el => el.classList.remove('active'))
+            this.classList.add("active")
+        }
+
+    }
+
+    articles.forEach(question => {
+        question.addEventListener('click', openQuestion)
+    })
+
+})()
+
+
+
